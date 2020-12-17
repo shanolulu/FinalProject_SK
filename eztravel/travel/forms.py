9a3f7b652ea35b56848b43157104a991e8736fea
shanolulu <shanolulu97@gmail.com>
2020-12-17 11:09:51 +0900

    eztravel

--- eztravel/travel/forms.py
+++ eztravel/travel/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 class UploadImageForm(forms.Form):
+    # title = forms.CharField(max_length=50)
     image = forms.ImageField()
-
+    print(image)
 # class QuestionForm(forms.ModelForm):
 #     class Meta:
 #         model = Question
